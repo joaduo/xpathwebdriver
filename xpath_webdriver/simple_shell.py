d4cc73821a726ca621f9ca76ffbb7418961080e0
jduo <jduo@juju.com>
2017-01-19 08:11:59 -0300

    pass url optionally

--- xpath_webdriver/simple_shell.py
+++ xpath_webdriver/simple_shell.py
@@ -8,10 +8,11 @@ Code Licensed under MIT License. See LICENSE file.
 import rel_imp; rel_imp.init()
 from .logger import Logger
 from .simple_xpath_browser import SimpleXpathBrowser
+from argparse import ArgumentParser
 
 logger = Logger()
 
-def embed():
+def embed(url):
     """Call this to embed IPython at the current point in your program.
     """
     iptyhon_msg = ('Could not embed Ipython, falling back to ipdb'
@@ -19,6 +20,8 @@ def embed():
     ipdb_msg = ('Could not embed ipdb, falling back to pdb'
                 ' shell. Exception: %r')
     b = browser = SimpleXpathBrowser()
+    if url:
+        b.get(url)
     display_banner = "XpathBrowser in 'b' or 'browser' variables"
     try:
         from IPython.terminal.embed import InteractiveShellEmbed
@@ -37,5 +40,26 @@ def embed():
             pdb.set_trace()
 
 
+class XpathShellCommand(object):
+    def get_parser(self):
+        parser = ArgumentParser(description='Test XPaths via Selenium.')
+        parser.add_argument('url', nargs='?')
+#        self._add_smoothtest_common_args(parser)
+        return parser
+
+    def main(self, argv=None):
+        args = self.get_parser().parse_args(argv)
+        embed(args.url)
+
+
+def smoke_test_module():
+    c = XpathShellCommand()
+
+
+def main(argv=None):
+    XpathShellCommand().main(argv)
+
+
 if __name__ == "__main__":
-    embed()
+    main()
+
