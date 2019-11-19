a25fe60efe7adac8d7aa4a0eca1b92acfb50c4c4
jduo <jduo@juju.com>
2019-11-19 18:54:14 -0300

    Remove distracting options

--- examples/07_xpathshell_settings.py
+++ examples/07_xpathshell_settings.py
@@ -7,10 +7,4 @@ from xpathwebdriver.default_settings import Settings
 
 class Settings(Settings):
     virtual_display_enable = True # If true, put browser in a contained window
-    #virtual_display_backend = 'xvnc' # If you want to run in a remote server
-    virtual_display_size = (800, 600)
     virtual_display_visible = True # Useful only when backend is not xvnc
-    virtual_display_keep_open = True # If we want to check results (useful whe combined with webdriver_browser_keep_open)
-
-    webdriver_browser = 'Chrome'
-    webdriver_browser_keep_open = True #Survive or not after finishing
