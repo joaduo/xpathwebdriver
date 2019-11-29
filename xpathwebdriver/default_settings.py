f6211d5b28a67237620d19e01be053642aa3bd62
jduo <jduo@juju.com>
2019-11-29 19:09:32 -0300

    Parse correctly environment variables

--- xpathwebdriver/default_settings.py
+++ xpathwebdriver/default_settings.py
@@ -13,6 +13,22 @@ import os
 logger = logging.getLogger('default_settings')
 
 
+class ConfigVar(object):
+    def __init__(self, value, name=None, parser=None):
+        self.value = value
+        self.name = name
+        self.parser = parser or self._solve_parser()
+
+    def _solve_parser(self, value):
+        parser = type(value)
+        if parser == bool:
+            parser = eval
+        return parser
+
+    def parse(self, value_str):
+        return self.parser(value_str)
+
+
 class Settings(object):
     # Server to be tested URL eg: http://www.example.com 
     base_url = None
@@ -26,8 +42,8 @@ class Settings(object):
     # avoiding the browser to pop-up abover other windows (with alerts for example)
     virtual_display_enable = False # Use virtual display
     virtual_display_visible = False # Show the virtual display or may be hidden (for headless testing)
-    virtual_display_backend = None # 'xvfb', 'xvnc' or 'xephyr', ignores ``virtual_display_visible``
-    virtual_display_size = (800, 600) # Dimensions of the virtual display
+    virtual_display_backend = ConfigVar(None, parser=str) # 'xvfb', 'xvnc' or 'xephyr', if set then ignores `virtual_display_visible`
+    virtual_display_size = ConfigVar((800, 600), parser=eval) # Dimensions of the virtual display
     virtual_display_keep_open = False # If we want to check results (useful whe combined with webdriver_browser_keep_open)
 
     webdriver_enabled = True # Whether or not automatically create the browser
@@ -56,7 +72,6 @@ class Settings(object):
     assert_screenshots_failed_dir = '/tmp/'
 
     log_level_default = logging.INFO
-    log_level_root_handler = logging.DEBUG
     log_color = False # Not working on Python 3
 
     def __init__(self):
@@ -77,17 +92,21 @@ class Settings(object):
         Support loading from environment variables
         '''
         config_vars = self._get_config_vars()
-        for env_var, attr_name in config_vars.items():
+        for env_var, cfg_var in config_vars.items():
             if env_var in os.environ:
-                logger.debug('Using %s=%r => %s', env_var, os.environ[env_var], attr_name)
-                setattr(self, attr_name, os.environ[env_var])
+                logger.debug('Using %s=%r => %s', env_var, os.environ[env_var], cfg_var.name)
+                setattr(self, cfg_var.name, cfg_var.parse(os.environ[env_var]))
 
     def _get_config_vars(self):
         config = {}
         for n in dir(self):
             if n.startswith('_'):
                 continue
-            config['XPATHWD_' + n.upper()] = n
+            cfg_var = getattr(self, n)
+            if not isinstance(cfg_var, ConfigVar):
+                cfg_var = ConfigVar(cfg_var)
+            cfg_var.name = cfg_var.name or n
+            config['XPATHWD_' + n.upper()] = cfg_var
         return config
 
 
