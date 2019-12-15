7c2f4c50de3f5669b69f65425fb656d300e907fe
jduo <jduo@juju.com>
2019-12-15 11:38:07 -0300

    Remove unused level vars

--- xpathwebdriver/levels.py
+++ xpathwebdriver/levels.py
@@ -1,12 +1,12 @@
 
 # Webdriver browser life level constants
 
-# Browser survives across processes (INMORTAL_LIFE is deprecated name)
-SURVIVE_PROCESS = INMORTAL_LIFE = 5
+# Browser survives across processes
+SURVIVE_PROCESS = 5
 # Browser is alive as long as the python process is running
 PROCESS_LIFE = 4
-# Browser is alive as long as the WebdriverManager is not deleted (TEST_RUNNER_LIFE is deprecated name)
-MANAGER_LIFE = TEST_RUNNER_LIFE = 3
+# Browser is alive as long as the WebdriverManager is not deleted
+MANAGER_LIFE = 3
 # Browser is alive for a single test round (eg: a unit test class)
 TEST_ROUND_LIFE = 2
 # Start 1 new browser per test ran
