54caac00ed4b6f302ee3dc4466711e0737f18352
jduo <jduo@juju.com>
2019-12-22 02:11:38 -0300

    Fix failing test in Codeship

---
+++ xpathwebdriver_tests/wipe_alerts.py
@@ -0,0 +1,22 @@
+from xpathwebdriver_tests.test_XpathBrowser import WebUnitTestBase
+import unittest
+
+
+class TestXpathBrowserWipeAlerts(WebUnitTestBase):
+    def test_wipe_alerts(self):
+        from selenium.common.exceptions import UnexpectedAlertPresentException
+        body = '''
+          <script type="text/javascript">
+            alert('Example alert');
+          </script>
+        '''
+        try:
+            with self.create_html('test_wipe_alerts', body) as path:
+                self.get_local_page(path)
+        except UnexpectedAlertPresentException:
+            self.browser.wipe_alerts()
+        else:
+            self.fail('No alert wiped')
+            
+if __name__ == "__main__":
+    unittest.main()
