bc033b98d19acfb94210a519583cdbade99e6865
joaduo <joaduo@gmail.com>
2024-09-13 10:41:16 -0300

    run headless

--- run_gh_ci.sh
+++ run_gh_ci.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-sudo apt-get install -y findimagedupes
+set -e -x
+
+
+sudo apt-get install -y imagemagick findimagedupes xvfb chromium-browser chromium-chromedriver
+
 pip install -r requirements.tests.txt
 pip install -r requirements.txt
+
+export \
+XPATHWD_VIRTUAL_DISPLAY_ENABLED=True \
+XPATHWD_VIRTUAL_DISPLAY_VISIBLE=False
 
 bash run_tests.sh
