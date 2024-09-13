ca48f32d9ebd7686603bdb41f59522728363241a
joaduo <joaduo@gmail.com>
2024-09-13 09:55:59 -0300

    make CI work again

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+set -e -x
+
 pushd `dirname $0` > /dev/null
-    #pip install -r requirements.txt
     python -m unittest discover -s xpathwebdriver_tests
     python xpathwebdriver_tests/wipe_alerts.py
 popd > /dev/null
