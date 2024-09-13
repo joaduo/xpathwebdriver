37bec6acaafa31fc98b67e70abacbb0ef62424fb
joaduo <joaduo@gmail.com>
2024-09-13 12:36:06 -0300

    Fix test

--- run_tests.sh
+++ run_tests.sh
@@ -3,6 +3,7 @@
 set -e -x
 
 pushd `dirname $0` > /dev/null
+    export PYTHONPATH="$PYTHONPATH:./"
     python3 -m unittest discover -s xpathwebdriver_tests
     python3 xpathwebdriver_tests/wipe_alerts.py
 popd > /dev/null
