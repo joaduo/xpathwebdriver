52757e5b42ca1e09dda867e9aa6298e6a3cd2de3
joaduo <joaduo@gmail.com>
2024-10-14 01:40:15 -0300

    Remove rel_imp

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ name = 'xpathwebdriver'
 
 reqs = '''pyvirtualdisplay>=0.2.1
 selenium>=3.0.2
-rel_imp>=0.2.4
 parsel>=1.6.0'''.splitlines()
 
 
@@ -17,15 +16,15 @@ def long_description():
 setup(
   name = name,
   packages = find_packages(),
-  version = '2.0.5',
-  description = 'Simpler selenium/webdriver API through a wrapper',
+  version = '2.0.6',
+  description = 'Selenium/webdriver wrapper for XPath and CSS selection',
   long_description=long_description(),
   long_description_content_type='text/x-rst',
   author = 'Joaquin Duo',
   author_email = 'joaduo@gmail.com',
   license='MIT',
   url = 'https://github.com/joaduo/'+name,
-  keywords = ['testing', 'automation', 'web', 'unittest', 'webdriver', 'selenium'],
+  keywords = ['testing', 'automation', 'web', 'unittest', 'webdriver', 'selenium', 'xpath', 'css'],
   install_requires=reqs,
   scripts=[
            'xpathwebdriver/commands/xpathshell',
