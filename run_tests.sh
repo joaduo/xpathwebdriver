f68c4f57f621a0685b17e346057c2fa692d830fe
jduodp <joaquin.duo@dialpad.com>
2026-02-06 18:24:28 -0300

    Skip ImagesComparator test on macOS, run specific test files instead of discover

--- run_tests.sh
+++ run_tests.sh
@@ -4,6 +4,9 @@ set -e -x
 
 pushd `dirname $0` > /dev/null
     export PYTHONPATH="$PYTHONPATH:./"
-    python3 -m unittest discover -s xpathwebdriver_tests
+    if ! [ "$(uname)" = "Darwin" ]; then
+        python3 xpathwebdriver_tests/test_ImagesComparator.py
+    fi
+    python3 xpathwebdriver_tests/test_XPathBrowser.py
     python3 xpathwebdriver_tests/wipe_alerts.py
 popd > /dev/null
--- run_tests_full.sh
+++ run_tests_full.sh
@@ -3,8 +3,8 @@
 set -e -x
 
 pushd `dirname $0` > /dev/null
-    python3 -m unittest discover -s xpathwebdriver_tests
-    python3 xpathwebdriver_tests/wipe_alerts.py #fails miserably on PhantomJs in Codeship
+    export PYTHONPATH="$PYTHONPATH:./"
+    bash run_tests.sh
     echo "Also running examples"
     python3 examples/01_duckduckgo_basic.py
     python3 examples/03_new_browser_per_test.py
