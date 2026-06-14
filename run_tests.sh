c35c7ba7f07e39e78a56ff4652fe74b88d6294e9
jduodp <joaquin.duo@dialpad.com>
2026-06-14 01:01:39 -0300

    Move PYTHONPATH export before pushd to apply globally across all test executions

--- run_tests.sh
+++ run_tests.sh
@@ -2,15 +2,15 @@
 
 set -e -x
 
+export PYTHONPATH="$PYTHONPATH:./"
+
 pushd `dirname $0` > /dev/null
-    export PYTHONPATH="$PYTHONPATH:./"
     if ! [ "$(uname)" = "Darwin" ]; then
         python3 xpathwebdriver_tests/test_ImagesComparator.py
     fi
 popd > /dev/null
 
 pushd `dirname $0` > /dev/null
-    export PYTHONPATH="$PYTHONPATH:./"
     python3 xpathwebdriver_tests/test_XPathBrowser.py
     python3 xpathwebdriver_tests/wipe_alerts.py
 popd > /dev/null
